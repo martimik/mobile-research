da98191ad799a1d3b00284dc89cce0c32e250780
L4721 <L4721@student.jamk.fi>
2019-10-19 18:06:36 +0300

    add statemachine to player

--- Player.js
+++ Player.js
@@ -3,18 +3,51 @@ import { StyleSheet, TouchableOpacity, View, Image } from 'react-native'
 import Icon from 'react-native-vector-icons'
 
 export default class Player extends React.Component{
+    constructor(props){
+        var isPlaying = false;
+
+    }
+    
+    // pass track name 
+    playTrack(track) {        
+        var track = new Sound(track, Sound.MAIN_BUNDLE, (error) => {
+            if(error){
+                console.log('failed to load the sound', error);
+                return;
+            }
+
+            console.log('Duration ' + track.getDuration())
+            if(isPlaying){
+                track.stop();
+            }else{
+                track.play();
+            }
+            
+        });
+    }
+
     render(){
         return(<Fragment>
             <View>
                 <Text>Player</Text>
             </View>
             <View>CD info/png</View>
+            <View>timeline</View>
             <View>
-                <Icon type="stepbackward" />
-                <Icon type="play" />
-                <Icon type="stepforward" />
+                <Icon type="controller-jump-to-start" />
+                {this.state.isPlaying && (
+                    <Icon type="controller-play" onPress={this.playTrack('test.mp3')} />
+                )}
+
+                {!this.state.isPlaying && (
+                    <Icon type="controller-paus" />
+                )}
+
+                <Icon type="controller-next" />
             </View>
         </Fragment> 
         );       
-    }    
+    }   
+    
+    
 }
