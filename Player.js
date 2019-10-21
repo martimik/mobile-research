8b1bb959675cb7ec9e12509af9e31a9526f59082
Samuli Virtapohja <samuli.virtapohja@gmail.com>
2019-10-21 10:07:55 +0300

    clean player

--- Player.js
+++ Player.js
@@ -1,18 +1,20 @@
 import React, {Fragment} from 'react';
-import { StyleSheet, TouchableOpacity, View, Image } from 'react-native'
+import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native'
 import Icon from 'react-native-vector-icons'
 import styles from './Styles.js';
 
 export default class Player extends React.Component{
+
     constructor(props){
-        var isPlaying = false;
-        var track = "";
-        var trackDuration = 0;
+        super(props)
+        //var isPlaying = false;
+        //var track = "";
+        //var trackDuration = 0;
     }
     
     // pass track name 
-    playTrack() {        
-        var track = new Sound(this.state.track, Sound.MAIN_BUNDLE, (error) => {
+    playTrack() { /*
+        var track = new Sound("biisi.mp3", Sound.MAIN_BUNDLE, (error) => {
             if(error){
                 console.log('failed to load the sound', error);
                 return;
@@ -27,33 +29,18 @@ export default class Player extends React.Component{
                 track.play();
                 isPlaying = true;
             }
-            
-        });
+
+        });*/
     }
 
     render(){
-
         return(
         <Fragment>
             <View style={styles.header}>
-                <Text style={styles.title}>Player</Text>
+                <Text>Player</Text>
             </View>
-            <View style={styles.content}>
-                <View >{this.state.track}</View>
-                <View>{this.state.trackDuration}</View>
-            </View>            
-            <View>
-                <Icon type="controller-jump-to-start" />
-                {this.state.isPlaying && (
-                    <Icon type="controller-play" onPress={this.playTrack()} />
-                )}
 
-                {!this.state.isPlaying && (
-                    <Icon type="controller-paus" onPress={this.playTrack()} />
-                )}
 
-                <Icon type="controller-next" />
-            </View>
         </Fragment> 
         );       
     }   
